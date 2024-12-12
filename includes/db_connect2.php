83b7b43b4c52e915e1abdb424bdfc24ad2002673
3sf00r <153434346+3sf00r@users.noreply.github.com>
2024-12-12 05:11:42 +0300

    Update db_connect2.php

--- includes/db_connect2.php
+++ includes/db_connect2.php
@@ -1,4 +1,16 @@
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "booking_sys";
+<?php
+function db_connect() {
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "booking_sys";
+
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    return $conn;
+}
+?>

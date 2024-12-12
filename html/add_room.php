01a3bd219b2beaabf0c4a136483401e9618b61bb
MahmoodFadhel352 <154272860+MahmoodFadhel352@users.noreply.github.com>
2024-12-12 05:32:18 +0300

    Update add_room.php

--- html/add_room.php
+++ html/add_room.php
@@ -1,1 +1,24 @@
+<?php
+session_start();
+include '../includes/db_connect.php';
+include '../includes/functions.php';
+include '../includes/header.php';
 
+if (!isAdmin()) {
+    header('Location: index.php');
+    exit;
+}
+
+$departments = fetchDepartments();
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+
+    header('Location: admin.php');
+    exit;
+}
+?>
+
+    <title>Add Room by Department</title>
+    <div class="main">
+        <h2>Add Room</h2>

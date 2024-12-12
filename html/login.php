5ff7f9e1c1ba06095301319503b7d96798b0c9ee
MahmoodFadhel352 <154272860+MahmoodFadhel352@users.noreply.github.com>
2024-12-12 03:57:00 +0300

    Update login.php

--- html/login.php
+++ html/login.php
@@ -3,17 +3,43 @@
 session_start();
 include 'includes/db_connect.php';
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>UOB Booking System</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="css/style.css">
+    <title >UOB booking system</title>
+
+    <div class="main2">
+<div class="container">
+<?php 
+
+if (isAdmin2()) {
+    echo '<a href="admin_dashboard.php"><img src="../img/logo.png" alt="logo" width="150px" height="150px"></a>';
+}else{
+        if (isset($_SESSION['user_id'])) {
+                echo '<a href="dashboard.php"><img src="../img/logo.png" alt="logo" width="150px" height="150px"></a>';}
+    else{
+        echo'<a href="index.php"><img src="../img/logo.png" alt="logo" width="150px" height="150px"></a>';
+        }
+    }
+?>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="../css/style.css"> 
+</div>
 </head>
 <body>
 
+<div >
+    <h1>UOB booking system</h1>
+    <?php if (isset($_SESSION['user_id'])): ?>
+        <a href="profile.php">Profile</a> |
+        <a href="logout.php">Logout</a>
+    <?php else: ?>
+        <a class="about" href="aboutUS.php">About US</a>
+    <?php endif; ?>
+</div>
 <div class="container">
         <h2>Login</h2>
         <form method="POST">

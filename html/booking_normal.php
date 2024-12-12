e74dd8c869859c397eba76de57c4a4f2b74fe5d6
A-BH75 <amarzooq10@gmail.com>
2024-12-12 05:22:47 +0300

    Update booking_normal.php

--- html/booking_normal.php
+++ html/booking_normal.php
@@ -4,10 +4,16 @@ include '../includes/header.php';
 include '../includes/db_connect.php';
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {}
+
+$error = '';
+
+$user_id = $_SESSION['user_id'];
+$user_email = $_SESSION['user_email'];  
+  
   ?>
 
 <div class="container">
-    <h2>Book a Room</h2>
+<h2>Book a Room</h2>
 
 </div>
 

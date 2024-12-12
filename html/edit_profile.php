b60913c4ce755ee519f72a4f7fdae4b805d9f64e
3sf00r <153434346+3sf00r@users.noreply.github.com>
2024-12-12 06:07:37 +0300

    Update edit_profile.php

--- html/edit_profile.php
+++ html/edit_profile.php
@@ -25,60 +25,28 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
         $hashed_password = hashPassword($password);
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title >UOB booking system</title>
-    <div class="main2">
-    <div class="container">
-        <?php 
-            if (isAdmin2()) {
-                    echo '<a href="admin_dashboard.php"><img src="../img/logo.png" alt="logo" width="150px" height="150px"></a>';
-                            }else{
-            if (isset($_SESSION['user_id'])) {
-                echo '<a href="dashboard.php"><img src="../img/logo.png" alt="logo" width="150px" height="150px"></a>';
-            }
-	        else{
-		        echo'<a href="index.php"><img src="../img/logo.png" alt="logo" width="150px" height="150px"></a>';
-                }
-            }    
-?>
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link rel="stylesheet" href="../css/style.css"> 
-    </div>	
-</head>
-<body>
-<div >
-    <h1>UOB booking system</h1>
-    <?php if (isset($_SESSION['user_id'])): ?>
-        <a href="profile.php">Profile</a> |
-        <a href="logout.php">Logout</a>
-    <?php else: ?>
-        <a class="about" href="aboutUS.php">About US</a>
-    <?php endif; ?>
-</div>
-    
-    <div class="container">
-        
-        <h2>Register</h2>
-        <form method="POST" enctype="multipart/form-data">
+<a href="profile.php" class="btn btn-primary">Back</a>
+<div class="main">
+    <h2>Edit Profile</h2>
+    <form id="profileForm" method="POST" action="edit_profile.php" enctype="multipart/form-data">
+        <div class="form-group">
             <label for="name">Name:</label>
-            <input type="text" id="name" name="name" required><br><br>
-
-            <label for="email">Email:</label>
-            <input type="email" id="email" name="email" required><br><br>
-
-            <label for="password">Password:</label>
-            <input type="password" id="password" name="password" required><br><br>
-
-            <label for="profile_picture">Profile Picture:</label>
-            <input type="file" id="profile_picture" name="profile_picture"><br><br>
-
-            <button type="submit">Register</button>
-        </form>
-    </div>
+            <input type="text" id="name" name="name" value="name" required>
+        </div>
+        <div class="form-group">
+            <label for="password">New Password (leave blank if not changing):</label>
+            <input type="password" id="password" name="password" required>
+        </div>
+        <div class="form-group">
+            <label for="confirmPassword">Confirm New Password:</label>
+            <input type="password" id="confirmPassword" name="confirmPassword" required>
+        </div>
+        <div class="form-group">
+            <label for="profile_picture">Upload Profile Picture (leave blank if not changing):</label>
+            <input type="file" id="profile_picture" name="profile_picture" accept="image/*" >
+        </div>
+        <button type="submit">Update Profile</button>
+    </form>
 </div>
     
 <?php include '../includes/footer.php'; ?>

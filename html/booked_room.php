124a5c6abccba73f5bb63ef370fe814a43941117
A-BH75 <amarzooq10@gmail.com>
2024-12-12 06:34:41 +0300

    Update booked_room.php

--- html/booked_room.php
+++ html/booked_room.php
@@ -3,4 +3,24 @@ if (!isset($_SESSION['user_id'])) {
     header("Location: index.php");
     exit();
 } ?>
+    <div class="main">
+    <h2>Booked Room Details</h2>
+
+    <?php 
+        $user_email = $_SESSION['user_email'];
+        $user_id = $_SESSION['user_id'];
+
+    ?>
+
+
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>Room Name</th>
+                    <th>Type</th>
+                    <th>Date</th>
+                    <th>Time</th>
+                    <th>Status</th>
+                    <th>Action</th>
+                </tr>
 <?php include '../includes/footer.php'; ?>

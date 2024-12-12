6a5637d2a3116e47f4c77e7e63ae73e06d68a2f9
3sf00r <153434346+3sf00r@users.noreply.github.com>
2024-12-12 06:50:45 +0300

    Update profile.php

--- html/profile.php
+++ html/profile.php
@@ -7,22 +7,25 @@ if (!isset($_SESSION['user_id'])) {
 include '../includes/header.php';
 include '../includes/db_connect.php';
 
+
 $stmt = $pdo->prepare("SELECT * FROM users WHERE id = :id");
 $stmt->execute(['id' => $_SESSION['user_id']]);
 $user = $stmt->fetch(PDO::FETCH_ASSOC);
 ?>
 
-<title>Profile</title>
+    <title>Profile</title>
 <div class="mainprofile"> 
 <h2>Your Profile</h2>
     
-<div class=" image d-flex flex-column justify-content-center align-items-center"> 
+		
+	 
+		<div class=" image d-flex flex-column justify-content-center align-items-center"> 
 			 
-	<img id="photo" src="<?= htmlspecialchars($user['profile_picture']) ?>" />
-			<span class="name mt-3"><?= htmlspecialchars($user['name']) ?></span> 
-				<span class="idd"><?= htmlspecialchars($user['email']) ?></span> 
-					<div class=" d-flex mt-2"> <a href="edit_profile.php" class="glass-btn">Edit Profile</a>
-</div> </div> </div>
+				<img id="photo" src="<?= htmlspecialchars($user['profile_picture']) ?>" />
+				 <span class="name mt-3"><?= htmlspecialchars($user['name']) ?></span> 
+					<span class="idd"><?= htmlspecialchars($user['email']) ?></span> 
+									<div class=" d-flex mt-2"> <a href="edit_profile.php" class="glass-btn">Edit Profile</a>
+									 </div> </div> </div>
 
         
  

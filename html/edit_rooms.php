b1a3b6ae5e4476d8890cbf7b79200bc700889620
MahmoodFadhel352 <154272860+MahmoodFadhel352@users.noreply.github.com>
2024-12-12 05:58:09 +0300

    Update edit_rooms.php

--- html/edit_rooms.php
+++ html/edit_rooms.php
@@ -1,1 +1,17 @@
+<?php
+session_start();
+include '../includes/db_connect2.php'; 
+include '../includes/functions.php'; 
+include '../includes/header.php';
 
+if (!isAdmin()) {
+    header('Location: index.php');
+    exit;
+}
+
+if (isset($_POST['delete_room'])) {
+    deleteRoom($_POST['delete_room']);
+    header('Location: delete_rooms.php');
+    exit;
+}
+?>

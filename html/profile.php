07b922490e279675e7ebff5b8f1d6126d3b3eff5
A-BH75 <amarzooq10@gmail.com>
2024-12-12 06:03:21 +0300

    Update profile.php

--- html/profile.php
+++ html/profile.php
@@ -5,5 +5,21 @@ if (!isset($_SESSION['user_id'])) {
     exit();
 }
 include '../includes/header.php';
->?
+include '../includes/db_connect.php';
+
+// getting user data from the database
+
+?>
+
+    <title>Profile</title>
+<div class="mainprofile"> 
+<h2>Your Profile</h2>
+
+
+
+        <div class=" image d-flex flex-column justify-content-center align-items-center"> 
+
+
+
+ 
 <?php include '../includes/footer.php'; ?>

cbdff14d206388d0bca80b3222f69842a71c9f5a
A-BH75 <amarzooq10@gmail.com>
2024-12-12 05:33:19 +0300

    Update add_room.php

--- html/add_room.php
+++ html/add_room.php
@@ -17,8 +17,18 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     header('Location: admin.php');
     exit;
 }
+    
 ?>
 
     <title>Add Room by Department</title>
     <div class="main">
         <h2>Add Room</h2>
+<form method="POST" action="">
+        <div class="form-group">
+            <label for="department">Department:</label>
+            <select name="department" class="select" required>
+                <?php foreach ($departments as $dept) { ?>
+                    <option value="<?php echo $dept['id']; ?>"><?php echo $dept['name']; ?></option>
+                <?php } ?>
+            </select>
+        </div>

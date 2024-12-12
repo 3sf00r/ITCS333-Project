38f7547894f588901a5b7993a3b86ea2dc6bdff6
MahmoodFadhel352 <154272860+MahmoodFadhel352@users.noreply.github.com>
2024-12-12 05:10:23 +0300

    Update rooms.php

--- html/rooms.php
+++ html/rooms.php
@@ -1,1 +1,12 @@
+<?php
+session_start();
+include '../includes/header.php';
+include '../includes/functions.php';
+if (!isset($_SESSION['user_id'])) {
+    header("Location: index.php");
+    exit();
+}
+include '../includes/db_connect2.php';
 
+$departments = fetchDepartments();
+?>

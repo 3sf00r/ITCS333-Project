e926aa18a280d5646aa5235612ae316068546cee
3sf00r <153434346+3sf00r@users.noreply.github.com>
2024-12-12 04:48:40 +0300

    Update Dashboard.php

--- html/Dashboard.php
+++ html/Dashboard.php
@@ -4,14 +4,29 @@
 <html lang="en">
 <head>
 <meta charset="UTF-8">
-
+<div class="navbar">
+    <a href="booking_normal.php" class="navbar-link">Booking</a>
+    <a href="rooms.php" class="navbar-link">Rooms</a>
+</div>
 </head>
 <body>
+        <?php
 if (!isset($_SESSION['user_id'])) {
         header("Location: login.php");
         exit();
     }
 
     $user_id = $_SESSION['user_id'];
+?>
+
+<div class="row">
+<div class="col-md-6">
+        <h3>Upcoming Bookings</h3>
+            <ul>
+                <?php foreach ($upcoming_bookings as $booking): ?>
+                    <li><?php echo $booking['room_name']; ?> - Date: <?php echo date('F j, Y', strtotime($booking['start_time'])); ?></li>
+                <?php endforeach; ?>
+            </ul>
+        </div>
 </body>
 <?php include '../includes/footer.php'; ?>

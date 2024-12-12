e97944260f3c20b536835e343c977dcff0e7490d
A-BH75 <amarzooq10@gmail.com>
2024-12-12 05:27:39 +0300

    Update admin.php

--- html/admin.php
+++ html/admin.php
@@ -1,1 +1,20 @@
+<?php
+session_start();
+include '../includes/db_connect2.php'; 
+include '../includes/functions.php'; 
+include '../includes/header.php'; 
 
+if (!isAdmin()) {
+    header('Location: index.php');
+    exit;
+}
+?>
+
+
+    <div class="container">
+        <h2>Welcome to the Admin Dashboard</h2>
+        <a href="delete_rooms.php" class="glass-btn">delete Rooms</a>
+        <a href="add_room.php" class="glass-btn">add Rooms</a>comments</a>
+        <a href="analytics.php" class="glass-btn">Analytics</a>
+    </div>
+<?php include '../includes/footer.php'; ?>

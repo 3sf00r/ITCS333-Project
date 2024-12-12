f4f0a43062b016f1a0285fbe55c1d96a8d7839d9
3sf00r <153434346+3sf00r@users.noreply.github.com>
2024-12-12 05:30:06 +0300

    final admin.php

--- html/admin.php
+++ html/admin.php
@@ -9,12 +9,14 @@ if (!isAdmin()) {
     exit;
 }
 ?>
+    
+<div class="container">
+    <h2>Welcome to the Admin Dashboard</h2>
+    <a href="delete_rooms.php" class="glass-btn">delete Rooms</a>
+    <a href="add_room.php" class="glass-btn">add Rooms</a>
+    <a href="edit_rooms.php" class="glass-btn">edit Rooms</a>
+    <a href="admin_comments.php" class="glass-btn">comments</a>
+    <a href="analytics.php" class="glass-btn">Analytics</a>
+</div>
 
-
-    <div class="container">
-        <h2>Welcome to the Admin Dashboard</h2>
-        <a href="delete_rooms.php" class="glass-btn">delete Rooms</a>
-        <a href="add_room.php" class="glass-btn">add Rooms</a>comments</a>
-        <a href="analytics.php" class="glass-btn">Analytics</a>
-    </div>
 <?php include '../includes/footer.php'; ?>

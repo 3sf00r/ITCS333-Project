7903b81735aba265f8af870cb07dc8b6be85740a
3sf00r <153434346+3sf00r@users.noreply.github.com>
2024-12-12 05:34:41 +0300

    final add_room.php
    final

--- html/add_room.php
+++ html/add_room.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-include '../includes/db_connect.php';
+include '../includes/db_connect2.php';
 include '../includes/functions.php';
 include '../includes/header.php';
 
@@ -12,18 +12,22 @@ if (!isAdmin()) {
 $departments = fetchDepartments();
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $department_id = $_POST['department'];
+    $name = htmlspecialchars($_POST['name']);
+    $capacity = intval($_POST['capacity']);
+    $equipment = htmlspecialchars($_POST['equipment']);
+    $type = htmlspecialchars($_POST['type']);
 
-
+    insertRoom($department_id, $name, $capacity, $equipment, $type);
     header('Location: admin.php');
     exit;
 }
-    
 ?>
 
-    <title>Add Room by Department</title>
-    <div class="main">
-        <h2>Add Room</h2>
-<form method="POST" action="">
+<title>Add Room by Department</title>
+<div class="main">
+    <h2>Add Room</h2>
+        <form method="POST" action="">
         <div class="form-group">
             <label for="department">Department:</label>
             <select name="department" class="select" required>
@@ -32,3 +36,26 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 <?php } ?>
             </select>
         </div>
+        <div class="form-group">
+            <label for="name">Name:</label>
+                <input type="text"  id="name" name="name" required>
+        </div>
+        <div class="form-group">
+            <label for="capacity">Capacity:</label>
+                <input type="number" id="capacity" name="capacity" required>
+        </div>
+        <div class="form-group">
+            <label for="equipment">Equipment:</label>
+                <input type="text"id="equipment" name="equipment" required>
+        </div>
+        <div class="form-group">
+            <label for="type">Type:</label>
+            <select name="type" class="select" required>
+                <option value="class">class</option>
+                <option value="lab">lab</option>
+            </select>
+        </div>
+        <button type="submit" class="glass-btn">Add</button>
+        </form>
+    </div>
+<?php include '../includes/footer.php'; ?>

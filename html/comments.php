6a2cc2b8fc97c14b25d92240bc07c6600a37a89e
MahmoodFadhel352 <154272860+MahmoodFadhel352@users.noreply.github.com>
2024-12-12 06:17:51 +0300

    Update comments.php

--- html/comments.php
+++ html/comments.php
@@ -1,1 +1,6 @@
-
+<?php session_start(); include '../includes/header.php';
+if (!isset($_SESSION['user_id'])) {
+    header("Location: index.php");
+    exit();
+} ?>
+<?php include '../includes/footer.php'; ?>

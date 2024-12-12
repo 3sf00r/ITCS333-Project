c6a57b2bea474df0517559f13015ce45e073e6dd
A-BH75 <amarzooq10@gmail.com>
2024-12-12 06:18:42 +0300

    Update comments.php

--- html/comments.php
+++ html/comments.php
@@ -3,4 +3,11 @@ if (!isset($_SESSION['user_id'])) {
     header("Location: index.php");
     exit();
 } ?>
+    <tr>
+            <th>Room Name</th>
+            <th>Type</th>
+             <th>Date</th>
+             <th>Time</th>
+            <th>Action</th>
+    </tr>
 <?php include '../includes/footer.php'; ?>

9b3fbfa34093e871afa1afc8cece987c46e4596e
3sf00r <153434346+3sf00r@users.noreply.github.com>
2024-12-12 05:57:22 +0300

    Update delete_rooms.php

--- html/delete_rooms.php
+++ html/delete_rooms.php
@@ -1,1 +1,36 @@
+<?php
+session_start();
+include '../includes/db_connect2.php'; 
+include '../includes/functions.php'; 
+include '../includes/header.php';
 
+if (!isAdmin()) {
+    header('Location: index.php');
+    exit;
+}
+
+if (isset($_POST['delete_room'])) {
+    deleteRoom($_POST['delete_room']);
+    header('Location: delete_rooms.php');
+    exit;
+}
+?>
+    
+<title>Manage Rooms</title>
+    <div class="main">
+        <h2>Manage Rooms</h2>
+        <?php $rooms = fetchAllRooms(); ?>
+        <?php foreach ($rooms as $room): ?>
+            <div class="main2">
+                <div class="card-body">
+                    <h5><?php echo htmlspecialchars($room['name']); ?></h5>
+                    <p>Capacity: <?php echo htmlspecialchars($room['capacity']); ?></p>
+                    <form method="POST" action="manage_rooms.php">
+                        <input type="hidden" name="delete_room" value="<?php echo $room['id']; ?>">
+                        <button type="submit" class="btn btn-danger">Delete</button>
+                    </form>
+                </div>
+            </div>
+        <?php endforeach; ?>
+    </div>
+<?php include '../includes/footer.php'; ?>

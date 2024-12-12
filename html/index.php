63e1877d2c8c9af163b273f15812f34ec3c51838
A-BH75 <amarzooq10@gmail.com>
2024-12-12 03:35:33 +0300

    welcome

--- html/index.php
+++ html/index.php
@@ -3,8 +3,14 @@
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="../css/style.css"> 
 
-
-
+<div class="container">
+    <h1>Welcome to UOB Room Booking System</h1>
+    <p>This is the main home page. You can register, login, browse rooms, and book a room.</p>
+<div class="d-flex justify-content-center">
+        <a href="registration.php" class="glass-btn">Register</a>
+        <a href="login.php" class="glass-btn">Login</a>
+</div>
+</div>
 
 </div>
 <?php include '../includes/footer.php'; ?>

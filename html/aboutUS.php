ba8d0de43ea725d56c9d2030d2a269ee9c51b7d9
MahmoodFadhel352 <154272860+MahmoodFadhel352@users.noreply.github.com>
2024-12-12 04:42:34 +0300

    Update aboutUS.php

--- html/aboutUS.php
+++ html/aboutUS.php
@@ -10,3 +10,17 @@
     <title >UOB booking system</title>
     <div class="main2">
 <div class="container">
+<?php 
+function isAdmin2() {
+    return isset($_SESSION['user_id']) && $_SESSION['user_role'] === 'admin';
+}
+if (isAdmin2()) {
+    echo '<a href="admin_dashboard.php"><img src="../img/logo.png" alt="logo" width="150px" height="150px"></a>';
+}else{
+        if (isset($_SESSION['user_id'])) {
+                echo '<a href="dashboard.php"><img src="../img/logo.png" alt="logo" width="150px" height="150px"></a>';}
+	else{
+		echo'<a href="index.php"><img src="../img/logo.png" alt="logo" width="150px" height="150px"></a>';
+        }
+    }
+?>

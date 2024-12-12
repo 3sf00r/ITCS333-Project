d15fc461daa52863910ac6985d85814928ca533b
3sf00r <153434346+3sf00r@users.noreply.github.com>
2024-12-12 04:14:27 +0300

    Update registration.php
    adding registration form

--- html/registration.php
+++ html/registration.php
@@ -18,6 +18,24 @@ function isAdmin2() {
 </head>
 <body>
     <h1>UOB booking system</h1> 
+    <div class="container">
+    <h2>Register</h2>
+    <form method="POST" enctype="multipart/form-data"> 
+        <label for="name">Name:</label>
+        <input type="text" id="name" name="name" required><br><br>
+
+        <label for="email">Email:</label>
+        <input type="email" id="email" name="email" required><br><br>
+
+        <label for="password">Password:</label>
+        <input type="password" id="password" name="password" required><br><br>
+
+        <label for="profile_picture">Profile Picture:</label>
+        <input type="file" id="profile_picture" name="profile_picture"><br><br>
+
+        <button type="submit">Register</button>
+    </form>
+</div>
 </body>
 </html>
 <?php include '../includes/footer.php'; ?>

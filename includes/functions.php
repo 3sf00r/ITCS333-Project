c548b9c11a99cf0c94da9fa1f172813a6fb381ef
A-BH75 <amarzooq10@gmail.com>
2024-12-12 03:59:27 +0300

    Update functions.php

--- includes/functions.php
+++ includes/functions.php
@@ -2,4 +2,8 @@
 function hashPassword($password) {
     return password_hash($password, PASSWORD_DEFAULT);
 }
+
+function verifyPassword($password, $hashedPassword) {
+    return password_verify($password, $hashedPassword);
+}
 ?>
